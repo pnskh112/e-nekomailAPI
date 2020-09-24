16b8dedcc0f40267a4eda36c8c1d1ebeb51797b5
pnskh112 <57710123+pnskh112@users.noreply.github.com>
2020-09-24 19:02:30 +0900

    Update e-nekomail_Receive_api.sh

--- e-nekomail_Receive_api.sh
+++ e-nekomail_Receive_api.sh
@@ -2,7 +2,7 @@
 export DISPLAY='unix:0.0'
 
 # 認証キーセット
-OcpApimSubscriptionKey="xxxhogexxx"
+OcpApimSubscriptionKey="===hoge==="
 srcName="e-nekomail_Receive_api.sh"
 mailId=""
 mailStatusMonitorTimer=""
@@ -24,7 +24,7 @@ date_d=$3
 date_ymd="${date_y}-${date_m}-${date_d}"
 echo $date_ymd >> $log 2>&1
 
-# 受信一覧取得APIを実行。メールIDを取得
+# 受信一覧取得API実行
 echo "受信一覧取得API"
 echo $json
 echo "Access ReceiveListApi Start"
@@ -41,95 +41,80 @@ res=`curl  -X POST "$requesturi/sdms/mails/inbox/" \
       -H "Content-Type: application/json" \
       -d @- << EOF | jq
 {
+    "inboxMailStatus": "READ",
 	"sendDateFrom": "${date_ymd}T00:00:00.000Z",
 	"sendDateTo": "${date_ymd}T23:59:59.000Z",
 	"skip": 0,
-	"take": 10
+	"take": 1
 }
 EOF
 `
-
-
 echo ${res}
-
 echo "Access ReceiveListApi End"
-# メールID取得（仮）
-# mailId="{:id}"
-mailId=`echo ${res} | jq '.iwsdWebMail[].iwsdreceiver[0].id'`
+
+# メールID取得
+mailId=`echo ${res} | jq '.iwsdWebMail[].iwsdreceiver[].id'`
 echo ${mailId}
-for key in $(echo ${res} | jq '.iwsdWebMail[].iwsdreceiver[0].id'); do
+for key in $(echo ${res} | jq '.iwsdWebMail[].iwsdreceiver[].id'); do
 
-    echo "$requesturi/sdms/mails/inbox/$key" 
+    mailId=${key}
+    echo "$requesturi/sdms/mails/inbox/$mailId" 
 
-# 受信詳細情報取得API実行
-echo "Access ReceiveShow Start" >> $log 2>&1
-echo "$requesturi/sdms/mails/$mailId/resume/$fileId" >> $log 2>&1
-# リクエストボディの定義
-json=$(cat << EOS
-{
-  "receiverId": ${mailId}
-}
-EOS
-)
-# リクエストとレスポンスを表示するオプション付きのcurlコマンド
-# curl --verbose \
-#       -X GET \
-#       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
-#       -H "Content-Type: application/json" \
-#       -d "$json" "$requesturi/sdms/mails/$mailId/resume/$fileId" >/dev/null
-res2=`curl  -X GET "$requesturi/sdms/mails/inbox/$key" \
-      -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
-      -H "Content-Type: application/json" `
-echo $res2
-done
-# echo $(echo $responseJson | jq '.iwsdfile') 2>&1$log
-echo $responseJson | jq -r '.iwsdfile[].id' >> $log 2>&1
-echo "Access ReceiveShow End" >> $log 2>&1
-# ファイルID取得（仮）
-# fileId="{iwsdfile:[:id]}"
-fileId="1"
-fileName="text.txt"
-# 消すな！JSON形式読み込むコマンド
-# echo $(echo $responseJson | jq '.filesCount') >> filesCount
-# リクエストボディの定義
-json=$(cat << EOS
-{
-    "receiverId" : ${mailId},
-    "fileId" : ${fileId}
-}
-EOS
-)
-echo $responseJson | jq -r '.iwsdfile[].id' >> $fileId
-for key in $(echo $responseJson | jq -r '.iwsdfile[].id'); do
-    # ダウンロード API実行
-    echo "Access download Start" >> $log 2>&1
-# リクエストボディの定義
-json=$(cat << EOS
-{
-    "receiverId" : ${mailId},
-    "fileId" : ${key}
-}
-EOS
-)
-    echo $json >> $log 2>&1
-    # echo "$responseJson | jq -r '.iwsdfile[].id'" >> $log 2>&1
+    # 受信詳細情報取得API実行
     # リクエストとレスポンスを表示するオプション付きのcurlコマンド
     # curl --verbose \
     #       -X GET \
     #       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
     #       -H "Content-Type: application/json" \
-    #       -d "$json" "$requesturi/sdms/mails/inbox/${mailId}/attachment/${fileId}" >/dev/null
-    # curl  -X GET \
-    #       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
-    #       -H "Content-Type: application/json" \
-    #       -d "$json" \
-    #       -O "$requesturi/sdms/mails/inbox/${mailId}/attachment/${fileId}/" >> $log 2>&1
-    curl  -X GET  "$requesturi/sdms/mails/inbox/${mailId}/attachment/${fileId}/" \
-          -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
-          -H "Content-Type: application/json" \
-          -d "$json">> $log 2>&1
-    
-    echo "Access download End" >> $log 2>&1
+    #       -d "$json" "$requesturi/sdms/mails/$mailId/resume/$fileId" >/dev/null
+    res2=`curl  -X GET "$requesturi/sdms/mails/inbox/$mailId" \
+          -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey"
+         `
+    echo $res2
+    echo "==="
+    echo "Access ReceiveShow End"
+
+    #ファイル名を配列に作成 
+    arrayFileName=()
+    for fileName in $(echo ${res2} | jq '.iwsdfile[].fileName'); do
+        arrayFileName+=(${fileName})
+    done
+
+    i=0
+    for fileId in $(echo $res2 | jq -r '.iwsdfile[].id');
+    do
+        echo "---i---"
+        echo $fileId
+        echo "---array---"
+        echo $arrayFileName[$i]
+
+
+        # ダウンロード API実行
+        echo "Access download Start"
+        echo "$requesturi/sdms/mails/inbox/${mailId}/attachment/${fileId}/"
+        
+        # echo "$responseJson | jq -r '.iwsdfile[].id'" >> $log 2>&1
+        # リクエストとレスポンスを表示するオプション付きのcurlコマンド
+        # curl --verbose \
+        #       -X GET \
+        #       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
+        #       -H "Content-Type: application/json" \
+        #       -d "$json" "$requesturi/sdms/mails/inbox/${mailId}/attachment/${fileId}" >/dev/null
+        # curl  -X GET \
+        #       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
+        #       -H "Content-Type: application/json" \
+        #       -d "$json" \
+        #       -O "$requesturi/sdms/mails/inbox/${mailId}/attachment/${fileId}/" >> $log 2>&1
+        curl  -X GET  "$requesturi/sdms/mails/inbox/${mailId}/attachment/${fileId}/" \
+              -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
+              -H "Content-Type: application/json" \
+              -o ${receive_dir}${arrayFileName[$i]}
+
+        # iをインクリメント
+        i=$((i + 1))        
+        echo "Access download End"
+    done
 done
 echo "${srcName} is end." >> $log 2>&1
 echo "------------------------------------------------" >> $log 2>&1
+exit 0
