49d5e75aaa35a22e89d685ef36f6c3036619929d
pnskh112 <57710123+pnskh112@users.noreply.github.com>
2020-09-28 18:11:29 +0900

    Update e-nekomail_Send_api.sh

--- e-nekomail_Send_api.sh
+++ e-nekomail_Send_api.sh
@@ -1,50 +1,51 @@
 #!/bin/bash
 export DISPLAY='unix:0.0'
+#*******************************************************************************
+# e-nekomail_Send.sh
+#
+#    概要        ： e-ネコセキュアデリバー　送信
+#
+#    履歴        ：2020/09/28 Create by S.Takeuchi
+#
+#*******************************************************************************
+
 
 # 認証キーセット
+OcpApimSubscriptionKey="---hoge---"
 srcName="e-nekomail_Send_api.sh"
-mailId=""
-filename="request.body"
-# 送信するZIPファイル名
-file="/home/dycsales/data/download/csv/yamato_senddata.zip"
-# file="\/home\/vagrant\/watir_src\/Ruby\/testmailaddress.txt"
-# file="testmail.txt"
+requesturi="https://fcms.i-securedeliver.jp"
 
-# fileName='"fileName" : "testmail.txt"'
-fileName='"fileName" : "request.body"'
+# 送信するZIPファイル
+file="/home/dycsales/data/download/csv/yamato_senddata.zip"
+
+# 送信時ファイル名をセット
+fileName='"fileName" : "yamato_senddata.zip"'
+
+# 送信時メールアドレス
+# mailAddress1=""
 mailAddress1="aaa@example.com"
 
-mailStatusMonitorTimer=""
-OcpApimSubscriptionKey="---hoge---"
-requesturi="https://fcms.i-securedeliver.jp"
-# log="/home/vagrant/watir_src/Ruby/logs/delive_export_logs/e-nekomail_Send.log"
-log="./e-nekomail_Send.log"
+# mailId初期値
+mailId=""
 
+# ログ出力用現在時刻取得
+logTime=`date +%Y%m%d_%H%M%S`
+
+log="/home/dycsales/data/logs/delive_export_logs/e-nekomail_Send_${logTime}.log"
+
+# メイン処理開始
 echo "------------------------------------------------" >> $log 2>&1
 echo "${srcName} is start." >> $log 2>&1
-# メール情報登録APIを実行。メールIDを取得
-echo $json >> $log 2>&1
-echo "Access mailApi Start" >> $log 2>&1
-echo "https://fcms.i-securedeliver.jp/sdms/mails/add" >> $log 2>&1
-# 引取期限セット
-        # 例）echo $responseJson | jq -r '.iwsdfile[].id' >> $fileId
-        # # 引き取り期限(1週間後を指定する例)
-        # days7=date"+%Y/%m/%d" -d "7 days"
-        
-        # # 返信期限(1カ月後を指定する例)
-        # month1=date"+%Y/%m/%d" -d "1 month"
 
-# リクエストボディの作成（attachedFildes,receivers）
-downloadDeadline="2020-09-26T00:00:00.000Z"
+# メール情報登録APIを実行。メールIDを取得
+echo "Access mailApi Start" >> $log 2>&1
+echo "$requesturi/sdms/mails/add" >> $log 2>&1
+
+# 引き取り期限(1週間後)セット
+downloadDeadline=`date "+%Y-%m-%dT00:00:00.000Z" -d "7 days"`
 
 # リクエストボディの定義
-# リクエストとレスポンスを表示するオプション付きのcurlコマンド
-# curl --verbose \
-#       -X GET \
-#       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
-#       -H "Content-Type: application/json" \
-#       -d "$json" "$requesturi/sdms/mails/history/1" >/dev/null
-res=`curl -X POST "https://fcms.i-securedeliver.jp/sdms/mails/add"\
+res=`curl -X POST "$requesturi/sdms/mails/add"\
       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
       -H "Content-Type: application/json" \
       --data-binary @- <<EOF | jq
@@ -69,43 +70,67 @@ res=`curl -X POST "https://fcms.i-securedeliver.jp/sdms/mails/add"\
 }
 EOF
 `
+# リクエストとレスポンスを表示するオプション付きのcurlコマンド(障害時のデバッグに利用)
+# res=`curl --verbose \
+#       -X POST "https://fcms.i-securedeliver.jp/sdms/mails/add"\
+#       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
+#       -H "Content-Type: application/json" \
+#       --data-binary @- <<EOF | jq
+# {
+#     "repositoryType": 2,
+#     "attachedFiles":[
+#         {
+#             ${fileName}
+#         }
+# 	],
+#     "downloadDeadline": "${downloadDeadline}",
+#     "downloadReminder": {},
+#     "language": "JAPANESE",
+#     "title": "testTitle",
+#     "comment": "Comment Test",
+#     "receivers": 
+#     [
+#         {
+#             "mailAddress1": "${mailAddress1}"
+#         }
+#     ]
+# }
+# EOF
+# `
 
-echo ${res}
-
-echo "Access mailApi End" >> $log 2>&1
 echo ${res} >> $log 2>&1
+echo "Access mailApi End" >> $log 2>&1
+
 # メールID取得
 mailId=`echo ${res} | jq '.id'`
 echo ${mailId} >> $log 2>&1
+
 # # ファイルID取得
 fileId=`echo ${res} | jq -r '.attachedFiles[].id'`
 echo ${fileId} >> $log 2>&1
+
 # ファイル登録API実行
 echo "Access fileApi Start" >> $log 2>&1
 echo "$requesturi/sdms/mails/$mailId/resume/$fileId" >> $log 2>&1
-# リクエストとレスポンスを表示するオプション付きのcurlコマンド
-# curl --verbose \
-#       -X GET \
-#       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
-#       -H "Content-Type: application/json" \
-#       -d "$json" "$requesturi/sdms/mails/$mailId/resume/$fileId" >/dev/null
 res2=`curl -X POST "$requesturi/sdms/mails/$mailId/resume/$fileId" \
       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
       -H "Content-Type: multipart/form-data" \
       -F "file=@$file"
 `
-echo ${res2json}
-echo ${res2}
+# リクエストとレスポンスを表示するオプション付きのcurlコマンド(障害時のデバッグに利用)
+# res2=`curl --verbose \
+#       -X POST "$requesturi/sdms/mails/$mailId/resume/$fileId" \
+#       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
+#       -H "Content-Type: multipart/form-data" \
+#       -F "file=@$file"
+# `
+
+echo ${res2} >> $log 2>&1
 echo "Access fileApi End" >> $log 2>&1
 echo "------------------------------------------------" >> $log 2>&1
+
 # タスク結果取得API実行
 echo "Access taskApi Start" >> $log 2>&1
-# リクエストとレスポンスを表示するオプション付きのcurlコマンド
-# curl --verbose \
-#       -X GET \
-#       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
-#       -H "Content-Type: application/json" \
-#       -d "$json" "$requesturi/sdms/mails/$mailId"  >/dev/null
 res3=`curl  -X GET "$requesturi/sdms/mails/history/$mailId"\
       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
       -H "Content-Type: application/json" \
@@ -115,14 +140,28 @@ res3=`curl  -X GET "$requesturi/sdms/mails/history/$mailId"\
 	}
 EOF
 `
+# リクエストとレスポンスを表示するオプション付きのcurlコマンド(障害時のデバッグに利用)
+# res3=`curl --verbose \
+#       -X GET "$requesturi/sdms/mails/history/$mailId"\
+#       -H "Ocp-Apim-Subscription-Key: $OcpApimSubscriptionKey" \
+#       -H "Content-Type: application/json" \
+#       --data-binary @- <<EOF | jq
+# 	{
+# 		"mailId": ${mailId}
+# 	}
+# EOF
+# `
+
 # ステータス取得
 status=`echo ${res3} | jq -r '.status'`
-echo ${res3}
-echo ${status}
+echo ${res3} >> $log 2>&1
+echo ${status} >> $log 2>&1
 if [ $status = "FINISHED" ]; then
     echo "status is FINISHED!" >> $log 2>&1
+else
+    return 1
 fi
 echo "Access taskApi End" >> $log 2>&1
 echo "${srcName} is end." >> $log 2>&1
 echo "------------------------------------------------" >> $log 2>&1
-
+return 0
